81fca857579b97496eb02482fd47f8b2d178aafe
Milo Vahos <milovahos@gmail.com>
2020-04-19 11:03:59 -0500

    Add days dynamically using momentjs

--- src/app/calendar/components/calendar/calendar.component.ts
+++ src/app/calendar/components/calendar/calendar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { Day } from '../../models/day.model';
 
 @Component({
   selector: 'app-calendar',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
+  momentCalendar = [];
+  myCalendar = [];
+
   constructor() { }
 
   ngOnInit() {
+    const startWeek = moment().startOf('month').week();
+    const endWeek = moment().endOf('month').week();
+    for ( let week = startWeek; week <= endWeek; week++ ) {
+      this.momentCalendar.push({
+        week,
+        days: Array(7).fill(0).map((n, i) => moment().week(week).startOf('week').clone().add(n + i, 'day')),
+      });
+    }
+    console.log(this.momentCalendar);
+    this.buildMyCalendar();
+    console.log(this.myCalendar);
   }
+
+  buildMyCalendar() {
+    this.momentCalendar.forEach( week => {
+      const w = [];
+      week.days.forEach( day => {
+        const d: Day = {
+          dayNumber: day.format('D'),
+          belonsCurrent: (day.month() === moment().month()) ? true : false,
+        };
+        w.push(d);
+      });
+      this.myCalendar.push(w);
+    });
+  }
+
 
 }
---
+++ src/app/calendar/models/day.model.ts
@@ -0,0 +1,4 @@
+export class Day {
+  public dayNumber: number;
+  public belonsCurrent: boolean;
+}
